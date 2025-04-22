52082428481b2a641caf74f135e046492e4c15ed
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2025-04-23 01:25:03 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Welcome to UserHub
+        </h2>
+        <p className="mt-2 text-center text-sm text-gray-600">
+          Sign in to manage your users
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
